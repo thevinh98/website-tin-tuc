67e442c36e29bb679d9d97055d9aa75ec45f7d7b
thevinh98 <55040942+thevinh98@users.noreply.github.com>
2019-12-13 22:41:24 +0700

    update footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -5,7 +5,7 @@
             <div class="col-md-12">
                 
                 <div style="text-align: center;">
-                   <p>Copyright &copy; Your Website tintuc</p> 
+                   <p>Copyright &copy; Your Website tintucfyfftytgyfg</p> 
                 </div>
             </div>
         </div>
